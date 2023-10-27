2214da478946c4e028837774e6b48b92f104707e
thahani80 <146523018+thahani80@users.noreply.github.com>
2023-10-27 15:18:44 +0530

    Delete 26-10 directory

--- 26-10/extension.py
+++
@@ -1,4 +0,0 @@
-filename=input("enter file name")
-exten=filename.split(".")
-print("The extension of file:",exten[-1])
-
--- 26-10/string.py
+++
@@ -1,3 +0,0 @@
-text=input("enter a string")
-newtext=text[-1]+text[1:-1]+text[0]
-print("new string:",newtext)
