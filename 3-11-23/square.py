a3f5b61dff9174f0a537767ecc04e6a5b482e633
thahani80 <146523018+thahani80@users.noreply.github.com>
2023-11-03 16:27:59 +0530

    Add files via upload

---
+++ 3-11-23/square.py
@@ -0,0 +1,23 @@
+def square(a,b):
+    list=[]
+    for i in range(a,b+1):
+        j=1;
+        while j*j<==1;
+            if j*j==1;
+                list.append(i)
+            j=j+1
+        i=i+1
+    return list
+print(square(1000,9999))
+list1=[]
+for i in list:
+    count=0
+    for z in 1:
+        if int(z)%2==0:
+            count=count+1
+        else:
+            break
+        if count==4:
+            list1.append(int(i))
+print(list1)
+    
